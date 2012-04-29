0dce01c609f78e37558ec815dd5aa9b443812ee7
Hindi <vct.Sutder@gmail.com>
2012-04-29 16:00:40 +0200

    Modif mineurs (ouais, je sais plus !)

--- menu.cpp
+++ menu.cpp
@@ -32,7 +32,7 @@ Menu::Menu(RenderWindow &app): m_app(app), position(250, 200), m_scale(1,1)
 
     position.y += 200;
     imageFadeCredit = new Image;
-    if(!(*imageFadeCredit).LoadFromFile("images/Crédits_fade.png"))
+    if(!(*imageFadeCredit).LoadFromFile("images/Credits_fade.png"))
     {
         cerr << "Image loading failed : nouvellePArtie_fade.png" << endl;
     }
@@ -46,28 +46,68 @@ Menu::Menu(RenderWindow &app): m_app(app), position(250, 200), m_scale(1,1)
     imagePlay = new Image;
     imageHow = new Image;
     imageCredit = new Image;
-
     if(!(*imagePlay).LoadFromFile("images/nouvellePartie.png"))
     {
             cerr << "Image loading failed : nouvellePArtie.png" << endl;
     }
+    if(!(*imageHow).LoadFromFile("images/CommentJouer.png"))
+    {
+            cerr << "Image loading failed : CommentJouer.png" << endl;
+    }
+    if(!(*imageCredit).LoadFromFile("images/Credits.png"))
+    {
+            cerr << "Image loading failed : Crédits.png" << endl;
+    }
+
+    imagePause = new Image;
+    imageReprendre = new Image;
+    imageArreter = new Image;
+
+    position.y = 0;
+    position.x = 0;
+    if(!(*imagePause).LoadFromFile("images/MenuPause.png"))
+    {
+            cerr << "Image loading failed : MenuPause.png" << endl;
+    }
     else
     {
-        if(!(*imageHow).LoadFromFile("images/CommentJouer.png"))
-        {
-                cerr << "Image loading failed : CommentJouer.png" << endl;
-        }
-        else
-        {
-            if(!(*imageCredit).LoadFromFile("images/Crédits.png"))
-            {
-                    cerr << "Image loading failed : Crédits.png" << endl;
-            }
-            else
-            {
+        spritePause.SetImage(*imagePause);
+        spritePause.SetPosition(position);
+        spritePause.Scale(m_scale);
+    }
+    if(!(*imageReprendre).LoadFromFile("images/ReprendreLaPartie.png"))
+    {
+            cerr << "Image loading failed : ReprendreLaPartie.png" << endl;
+    }
+    if(!(*imageArreter).LoadFromFile("images/QuitterTaPartie.png"))
+    {
+            cerr << "Image loading failed : QuitterTaPartie.png" << endl;
+    }
 
-            }
-        }
+    imageFadeReprendre = new Image;
+    imageFadeArreter = new Image;
+    position.y += 400;
+    position.x = 250;
+    if(!(*imageFadeReprendre).LoadFromFile("images/ReprendreLaPartie_fade.png"))
+    {
+            cerr << "Image loading failed : ReprendreLaPartie_fade.png" << endl;
+    }
+    else
+    {
+        spriteReprendre.SetImage(*imageFadeReprendre);
+        spriteReprendre.SetPosition(position);
+        spriteReprendre.Scale(m_scale);
+    }
+    position.y += 200;
+    if(!(*imageFadeArreter).LoadFromFile("images/QuitterTaPartie_fade.png"))
+    {
+            cerr << "Image loading failed : QuitterTaPartie_fade.png" << endl;
+    }
+    else
+    {
+        spriteArreter.SetImage(*imageFadeArreter);
+        spriteArreter.SetPosition(position);
+        spriteArreter.Scale(m_scale);
     }
 }
 
@@ -77,11 +117,16 @@ Menu::~Menu()
     delete imageHow;
     delete imageCredit;
     delete imageFadePlay;
-    delete imageFadePlay;
-    delete imageFadePlay;
+    delete imageFadeHow;
+    delete imageFadeCredit;
+    delete imagePause;
+    delete imageReprendre;
+    delete imageArreter;
+    delete imageFadeReprendre;
+    delete imageFadeArreter;
 }
 
-void Menu::draw(int select)
+void Menu::drawMainMenu(int select)
 {
     switch(select)
     {
@@ -112,3 +157,24 @@ void Menu::draw(int select)
     m_app.Draw(spriteHow);
 }
 
+void Menu::drawPauseMenu(int select)
+{
+    switch(select)
+    {
+        case 1:
+        {
+            spriteReprendre.SetImage(*imageReprendre);
+            spriteArreter.SetImage(*imageFadeArreter);
+            break;
+        }
+        case 2:
+        {
+            spriteReprendre.SetImage(*imageFadeReprendre);
+            spriteArreter.SetImage(*imageArreter);
+            break;
+        }
+    }
+     m_app.Draw(spritePause);
+     m_app.Draw(spriteReprendre);
+     m_app.Draw(spriteArreter);
+}
