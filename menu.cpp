a49255d64ed23e0f41db567f78bebcd0cdd77367
Hindi <vct.studer@gmail.com>
2012-09-17 00:38:12 +0200

    Utilisation du image manager dans le menu

--- menu.cpp
+++ menu.cpp
@@ -10,109 +10,63 @@ Menu::Menu(RenderWindow &app, image_manager imageManager):
             m_imageManager(imageManager)
 {
     imageFadePlay = new Image;
-    if(!(*imageFadePlay).LoadFromFile("images/nouvellePartie_fade.png"))
-    {
-        cerr << "Image loading failed : nouvellePartie_fade.png" << endl;
-    }
-    else
-    {
-        spritePlay.SetImage(*imageFadePlay);
-        spritePlay.SetPosition(position);
-        spritePlay.Scale(m_scale);
-    }
+    *imageFadePlay = m_imageManager.getImage("images/nouvellePartie_fade.png");
+    spritePlay.SetImage(*imageFadePlay);
+    spritePlay.SetPosition(position);
+    spritePlay.Scale(m_scale);
+
 
     position.y += 200;
     imageFadeHow = new Image;
-    if(!(*imageFadeHow).LoadFromFile("images/CommentJouer_fade.png"))
-    {
-        cerr << "Image loading failed : nouvellePArtie_fade.png" << endl;
-    }
-    else
-    {
-        spriteHow.SetImage(*imageFadeHow);
-        spriteHow.SetPosition(position);
-        spriteHow.Scale(m_scale);
-    }
+    *imageFadeHow = m_imageManager.getImage("images/CommentJouer_fade.png");
+    spriteHow.SetImage(*imageFadeHow);
+    spriteHow.SetPosition(position);
+    spriteHow.Scale(m_scale);
+
 
     position.y += 200;
     imageFadeCredit = new Image;
-    if(!(*imageFadeCredit).LoadFromFile("images/Credits_fade.png"))
-    {
-        cerr << "Image loading failed : nouvellePArtie_fade.png" << endl;
-    }
-    else
-    {
-        spriteCredit.SetImage(*imageFadeCredit);
-        spriteCredit.SetPosition(position);
-        spriteCredit.Scale(m_scale);
-    }
+    *imageFadeCredit = m_imageManager.getImage("images/Credits_fade.png");
+    spriteCredit.SetImage(*imageFadeCredit);
+    spriteCredit.SetPosition(position);
+    spriteCredit.Scale(m_scale);
+
 
     imagePlay = new Image;
     imageHow = new Image;
     imageCredit = new Image;
-    if(!(*imagePlay).LoadFromFile("images/nouvellePartie.png"))
-    {
-            cerr << "Image loading failed : nouvellePArtie.png" << endl;
-    }
-    if(!(*imageHow).LoadFromFile("images/CommentJouer.png"))
-    {
-            cerr << "Image loading failed : CommentJouer.png" << endl;
-    }
-    if(!(*imageCredit).LoadFromFile("images/Credits.png"))
-    {
-            cerr << "Image loading failed : Crédits.png" << endl;
-    }
-
-    imagePause = new Image;
-    imageReprendre = new Image;
-    imageArreter = new Image;
+    *imagePlay = m_imageManager.getImage("images/nouvellePartie.png");
+    *imageHow = m_imageManager.getImage("images/CommentJouer.png");
+    *imageCredit = m_imageManager.getImage("images/Credits.png");
 
     position.y = 0;
     position.x = 0;
-    if(!(*imagePause).LoadFromFile("images/MenuPause.png"))
-    {
-            cerr << "Image loading failed : MenuPause.png" << endl;
-    }
-    else
-    {
-        spritePause.SetImage(*imagePause);
-        spritePause.SetPosition(position);
-        spritePause.Scale(m_scale);
-    }
-    if(!(*imageReprendre).LoadFromFile("images/ReprendreLaPartie.png"))
-    {
-            cerr << "Image loading failed : ReprendreLaPartie.png" << endl;
-    }
-    if(!(*imageArreter).LoadFromFile("images/QuitterTaPartie.png"))
-    {
-            cerr << "Image loading failed : QuitterTaPartie.png" << endl;
-    }
+    imagePause = new Image;
+    imageReprendre = new Image;
+    imageArreter = new Image;
+    *imagePause = m_imageManager.getImage("images/MenuPause.png");
+    spritePause.SetImage(*imagePause);
+    spritePause.SetPosition(position);
+    spritePause.Scale(m_scale);
+
+    *imageReprendre = m_imageManager.getImage("images/ReprendreLaPartie.png");
+    *imageArreter = m_imageManager.getImage("images/QuitterTaPartie.png");
 
     imageFadeReprendre = new Image;
     imageFadeArreter = new Image;
     position.y += 400;
     position.x = 250;
-    if(!(*imageFadeReprendre).LoadFromFile("images/ReprendreLaPartie_fade.png"))
-    {
-            cerr << "Image loading failed : ReprendreLaPartie_fade.png" << endl;
-    }
-    else
-    {
-        spriteReprendre.SetImage(*imageFadeReprendre);
-        spriteReprendre.SetPosition(position);
-        spriteReprendre.Scale(m_scale);
-    }
+    *imageFadeReprendre = m_imageManager.getImage("images/ReprendreLaPartie_fade.png");
+    spriteReprendre.SetImage(*imageFadeReprendre);
+    spriteReprendre.SetPosition(position);
+    spriteReprendre.Scale(m_scale);
+
     position.y += 200;
-    if(!(*imageFadeArreter).LoadFromFile("images/QuitterTaPartie_fade.png"))
-    {
-            cerr << "Image loading failed : QuitterTaPartie_fade.png" << endl;
-    }
-    else
-    {
-        spriteArreter.SetImage(*imageFadeArreter);
-        spriteArreter.SetPosition(position);
-        spriteArreter.Scale(m_scale);
-    }
+    *imageFadeArreter = m_imageManager.getImage("images/QuitterTaPartie_fade.png");
+    spriteArreter.SetImage(*imageFadeArreter);
+    spriteArreter.SetPosition(position);
+    spriteArreter.Scale(m_scale);
+
 }
 
 Menu::~Menu()
