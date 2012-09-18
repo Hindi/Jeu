d4307b58f46d50b2505f51b2fe8eea5a7fb2b5c4
Studer <vct.studer@gmail.com>
2012-09-18 13:20:56 +0200

    Ajout de commentaires

--- enemies/boss.h
+++ enemies/boss.h
@@ -16,25 +16,57 @@ class Boss : public Unit
     public:
         Boss(int life, int scoreHit, int scoreExplosion,  int xSpeed, int ySpeed, const std::string &filepath, sf::Vector2f position, char* type, char* moveMethod,
              int moveValue, const int coefSpeed, const int firerate, sf::RenderWindow &app, Player &player, image_manager &imageManager, Projectile_manager &projectile_manager);
+        //Destruteur
         ~Boss();
 
     private:
+        //Le joueur
         Player &m_player;
+
+        //Liste des projectiles du boss
         std::list<Projectile*> m_projectiles;
+
+        //Liste des vitesses de tous les projectiles
         std::list<sf::Vector2f> m_vitesses;
+
+        //Direction de déplacement
         char* direction;
+
+        //Porjectile
         Projectile *projectile;
+
+        //Li'mage du boss
         sf::Image *image;
+
+        //Le moment du dernier tir
         float lastShot;
+
+        //Timers de tir et de déplacement
         Timer timer, timerMove;
+
+        //Constantes de score au touché et à la mort, vitesses des projectiles et fréquence de tir
         int const m_scoreHit, m_scoreExplosion, projectileSpeed, m_coefSpeed, m_fireRate;
+
+        //Sauvegarde sur un timer de déplacement
         float savedTimerMove;
+
+        //Variables d'animation
         Animated *m_animated;
         Anim m_anim;
+
+        //Manager d'images
         image_manager &m_imageManager;
+
+        //Position du jour
         sf::Vector2f playerPosition;
+
+        //Type de tir
         char* m_type;
+
+        //Type de déplacement
         char* m_moveMethod;
+
+        //Etape dans les différents déplacements
         int  m_moveValue;
 
 
